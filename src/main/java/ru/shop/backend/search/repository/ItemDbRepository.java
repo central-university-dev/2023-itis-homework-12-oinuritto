a949e3ff79d392ef0cca4d3477657520ddc01d23
oinuritto <ainursabitovak@mail.ru>
2023-12-12 00:12:08 +0300

    deleted empty strings in concatenation

--- src/main/java/ru/shop/backend/search/repository/ItemDbRepository.java
+++ src/main/java/ru/shop/backend/search/repository/ItemDbRepository.java
@@ -14,16 +14,13 @@ public interface ItemDbRepository  extends JpaRepository<ItemEntity, Long> {
             "where i.item_id in  :ids", nativeQuery = true)
     List<Object[]> findByIds(Integer regionId, List<Long> ids);
 
-    @Query(value = "" +
-            "select item_id from item_sku where sku = ?", nativeQuery = true)
+    @Query(value = "select item_id from item_sku where sku = ?", nativeQuery = true)
     List<Integer> findBySku(String parseInt);
 
-    @Query(value = "" +
-            "select i.* from item as i", nativeQuery = true)
+    @Query(value = "select i.* from item as i", nativeQuery = true)
     Stream<ItemEntity> findAllInStream();
 
-    @Query(value = "" +
-            "select distinct c.name, cp.name as parent_name, c.realcatname as url , cp.realcatname as parent_url,\n" +
+    @Query(value = "select distinct c.name, cp.name as parent_name, c.realcatname as url , cp.realcatname as parent_url,\n" +
             "c.image                                                                                             \n" +
             "from item as i                                                                          \n" +
             "join catalogue as c using(catalogue_id)                                                            \n" +
