705d5a77f76d233797dbb868f4e861db7bd10e2c
oinuritto <ainursabitovak@mail.ru>
2023-12-12 18:42:22 +0300

    refactored SearchController

--- src/main/java/ru/shop/backend/search/api/SearchController.java
+++ src/main/java/ru/shop/backend/search/api/SearchController.java
@@ -5,14 +5,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.shop.backend.search.model.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import ru.shop.backend.search.repository.ItemDbRepository;
+import ru.shop.backend.search.model.SearchResult;
+import ru.shop.backend.search.model.SearchResultElastic;
 import ru.shop.backend.search.service.SearchService;
-
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -21,6 +20,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Tag(name = "Поиск", description = "Методы поиска")
 @RequiredArgsConstructor
 public class SearchController {
+    private final SearchService service;
+
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Возвращает результаты поиска для всплывающего окна",
                 content = {@Content(mediaType = "application/json",
@@ -29,38 +30,23 @@ public class SearchController {
                 content = @Content),
         @ApiResponse(responseCode = "404", description = "Регион не найден",
                 content = @Content)})
-    @Parameter(name = "text", description = "Поисковый запрос")
     @GetMapping
-    public SearchResult find(@RequestParam String text, @CookieValue(name="regionId", defaultValue="1") int regionId){
-        return service.getSearchResult( regionId,  text);
+    public SearchResult find(@Parameter(description = "Поисковый запрос")
+                             @RequestParam String text,
+                             @CookieValue(name="regionId", defaultValue="1") int regionId){
+        return service.getSearchResult(regionId, text);
     }
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Возвращает результаты поиска",
                     content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = SearchResult.class))}),
+                            schema = @Schema(implementation = SearchResultElastic.class))}),
             @ApiResponse(responseCode = "400", description = "Ошибка обработки",
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Регион не найден",
                     content = @Content)})
-    @Parameter(name = "text", description = "Поисковый запрос")
     @RequestMapping(method = GET, value = "/by", produces = "application/json;charset=UTF-8")
-    public ResponseEntity finds(@RequestParam String text, @CookieValue(name="regionId", defaultValue="1") int regionId) {
-        if (service.isNumeric(text)) {
-            Integer itemId = itemDbRepository.findBySku(text).stream().findFirst().orElse(null);
-            if (itemId == null) {
-                var catalogue = service.getByName(text);
-                if (catalogue.size() > 0) {
-                    return ResponseEntity.ok().body(new SearchResultElastic(catalogue));
-                }
-                return ResponseEntity.ok().body(new SearchResultElastic(service.getAllFull(text)));
-            }
-            try {
-                return ResponseEntity.ok().body(new SearchResultElastic(service.getByItemId(itemId.toString())));
-            } catch (Exception e) {
-            }
-        }
-        return ResponseEntity.ok().body(new SearchResultElastic(service.getAllFull(text)));
+    public ResponseEntity<SearchResultElastic> finds(@Parameter(description = "Поисковый запрос")
+                                                     @RequestParam String text) {
+        return ResponseEntity.ok().body(service.getSearchResultElastic(text));
     }
-    private final ItemDbRepository itemDbRepository;
-    private final SearchService service;
 }
